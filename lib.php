343f832e1d46353668fdd80e9ec255aafc5dc223
RacemBenrhayem <racem.benrhayem@corolair.com>
2024-12-12 17:09:19 +0100

    Remove TODO comment regarding API endpoint configuration in lib.php

--- lib.php
+++ lib.php
@@ -60,7 +60,7 @@ function local_corolair_extend_navigation_course($navigation, $course, $context)
     }
 
     // URL for the POST request
-    $url = "https://services.corolair.com/moodle-integration/courses/instances/tutor"; // TODO: Should be moved to settings and use get_string('apiendpoint', 'local_corolair')
+    $url = "https://services.corolair.com/moodle-integration/courses/instances/tutor"; 
 
     // Get the API key from the configuration
     $apikey = get_config('local_corolair', 'apikey');
