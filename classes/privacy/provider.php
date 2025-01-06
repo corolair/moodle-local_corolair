d00d78dbad73b346cdda0e1bda282cc5b0cea7c5
RacemBenrhayem <racem.benrhayem@corolair.com>
2025-01-06 11:17:18 +0100

    Add privacy API methods documentation for user data export and deletion in local_corolair

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy Subsystem implementation for local_corolair.
+ * 
+ * @package   local_corolair
+ * @copyright  2024 Corolair 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 namespace local_corolair\privacy;
 
 use core_privacy\local\metadata\collection;
@@ -115,6 +138,16 @@ lc_userlist_provider{
         return $contextlist;
     }
 
+    /**
+     * Exports user data for the given approved context list.
+     *
+     * This function retrieves the API key from the configuration, constructs a URL to an external service,
+     * and sends a request to export user data. If the API key is not set or invalid, or if the request fails,
+     * the function returns without exporting any data. If the request is successful, the function decodes the
+     * JSON response and exports the data using Moodle's privacy API.
+     *
+     * @param approved_contextlist $approved_contextlist The list of approved contexts for the user.
+     */
     public static function export_user_data(approved_contextlist $approved_contextlist) {
         $apikey = get_config('local_corolair', 'apikey');
         if (!$apikey || strpos($apikey, get_string('noapikey', 'local_corolair')) === 0) {  
@@ -150,6 +183,12 @@ lc_userlist_provider{
         }
     }
 
+    /**
+     * Retrieves the list of users in a given context and adds them to the user list.
+     *
+     * @param userlist $userlist The user list object to which users will be added.
+     * @return void
+     */
     public static function get_users_in_context(userlist $userlist) {
         $apikey = get_config('local_corolair', 'apikey');
         if (!$apikey || strpos($apikey, get_string('noapikey', 'local_corolair')) === 0) {  
@@ -183,6 +222,12 @@ lc_userlist_provider{
         return;
     }
 
+    /**
+     * Deletes data for all users in the given context.
+     *
+     * @param \context $context The context from which to delete data.
+     * @return void
+     */
     public static function delete_data_for_all_users_in_context(\context $context) {
         $apikey = get_config('local_corolair', 'apikey');
         if (!$apikey || strpos($apikey, get_string('noapikey', 'local_corolair')) === 0) {  
@@ -206,6 +251,16 @@ lc_userlist_provider{
         return;
     }
 
+    /**
+     * Deletes data for a user based on the provided context list.
+     *
+     * This function retrieves the API key from the configuration and checks if it is valid.
+     * If the API key is not set or is invalid, the function returns without performing any action.
+     * Otherwise, it constructs a URL to the Corolair service to delete the user's data and sends
+     * a DELETE request to that URL.
+     *
+     * @param approved_contextlist $contextlist The context list containing the user whose data is to be deleted.
+     */
     public static function delete_data_for_user(approved_contextlist $contextlist) {
         $apikey = get_config('local_corolair', 'apikey');
         if (!$apikey || strpos($apikey, get_string('noapikey', 'local_corolair')) === 0) {  
@@ -224,8 +279,15 @@ lc_userlist_provider{
 
     }
     
-    
-
+    /**
+     * Deletes data for users specified in the approved user list.
+     *
+     * This function sends a DELETE request to the external Corolair service to delete user data.
+     * It retrieves the API key from the local configuration and constructs the request URL for each user.
+     * If the API key is not set or is invalid, the function returns without performing any action.
+     *
+     * @param approved_userlist $userlist The list of approved users whose data needs to be deleted.
+     */
     public static function delete_data_for_users(approved_userlist $userlist) {
         $apikey = get_config('local_corolair', 'apikey');
         if (!$apikey || strpos($apikey, get_string('noapikey', 'local_corolair')) === 0) {  
@@ -243,8 +305,6 @@ lc_userlist_provider{
             $response = curl_exec($ch);
             $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         }
-
         curl_close($ch);
-        
     } 
 }
